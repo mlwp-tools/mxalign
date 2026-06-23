2c7c905c18031fd913efb06c65badecd7dfaf4c2
Michiel Van Ginderachter <michiel.vanginderachter@meteo.be>
2026-06-23 12:33:16 +0000

    add custom loader

---
+++ examples/myloader.py
@@ -0,0 +1,21 @@
+import xarray as xr
+import dask
+
+def load_dataset(file):
+    ds = xr.open_mfdataset([file], engine='h5netcdf')
+
+    ds = ds.rename_dims(code="point_index")
+
+    ds.attrs["mlwp_time_trait"] = "observation"
+    ds.attrs["mlwp_space_trait"] = "point"
+    ds.attrs["mlwp_uncertainty_trait"] = "deterministic"
+    
+    ds.coords["valid_time"].attrs["standard_name"] = "time"
+    ds.coords["latitude"].attrs.update(
+        {"standard_name": "latitude", "units": "degrees_north"}
+    )
+    ds.coords["longitude"].attrs.update(
+        {"standard_name": "longitude", "units": "degrees_east"}
+    )
+
+    return ds
